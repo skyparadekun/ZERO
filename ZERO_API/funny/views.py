0ba9b1c2ad291489a31455f9ec59ac57c7a559e3
skyparadekun <823498472@qq.com>
2018-11-25 23:34:15 +0800

    2.0.3 unfinished lack json

--- ZERO_API/funny/views.py
+++ ZERO_API/funny/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import auth
-import json
+from django.http import JsonResponse
+from json import dumps
+from django.core import serializers
 from .models import *
 import datetime
 # Create your views here.
@@ -17,6 +19,7 @@ def signIn(request):
                 auth.login(request, user)
                 value['code']=1
                 value['status']='Success!'
+                value['info']=list(User.objects.get(username=username))
                 return HttpResponse(json.dumps(value),content_type="application/json")
         else:
             value['code']=0
@@ -68,8 +71,24 @@ def signOut(request):
         return HttpResponse(json.dumps(value),content_type="application/json")
 
 def getComment(request):
+    value={}
+    if request.method=="GET":
+        articleId = request.GET.get('articleId','')
+        article = Article.objects.get(id=articleId)
+        comments = Comment.objects.filter(article=article)
+        Info=[]
+        for comment in comments:
+            Info.append(model_to_dict(comment))
+        value['info']=Info
+        value['code']=1
+        value['status']='Success!'
+        return HttpResponse(json.dumps(value),content_type="application/json")
+    else:
+        value['code']=0
+        value['status']="Can't visit the API in this way!"
+        return HttpResponse(json.dumps(value),content_type="application/json")
 
-@login_required
+@login_required()
 def setComment(request):
     value={}
     if request.user.is_active:
@@ -92,5 +111,35 @@ def setComment(request):
         return HttpResponse(json.dumps(value),content_type="application/json")
 
 def getArticle(request):
+    value={}
+    if request.method=="GET":
+        movieId = request.GET.get('movieId','')
+        movie = Movie.objects.get(id=movieId)
+        articles = Article.objects.filter(movie=movie)
+        Info=[]
+        for article in articles:
+            Info.append(model_to_dict(article))
+        value['info']=Info
+        value['code']=1
+        value['status']='Success!'
+        return HttpResponse(json.dumps(value),content_type="application/json")
+    else:
+        value['code']=0
+        value['status']="Can't visit the API in this way!"
+        return HttpResponse(json.dumps(value),content_type="application/json")
 
 def getMovie(request):
+    value={}
+    if request.method=="GET":
+        movies = Movie.objects.all()
+        Info=[]
+        for movie in movies:
+            Info.append(model_to_dict(movie))
+        value['info']=Info
+        value['code']=1
+        value['status']='Success!'
+        return HttpResponse(json.dumps(value),content_type="application/json")
+    else:
+        value['code']=0
+        value['status']="Can't visit the API in this way!"
+        return HttpResponse(json.dumps(value),content_type="application/json")
